5b6cf384048c0b34c6beb7edbe390c6dbc4fcb5e
Yongsu Jung <kjlolo0@naver.com>
2014-11-26 16:23:17 +0900

    다시 올리기

---
+++ Application/Gilian/src/com/gilian/ese/inu/Map.java
@@ -0,0 +1,16 @@
+package com.gilian.ese.inu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Map extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.map);
+	    // TODO Auto-generated method stub
+	}
+
+}
---
+++ Application/Gilian/src/com/gilian/ese/inu/Navigation.java
@@ -0,0 +1,16 @@
+package com.gilian.ese.inu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Navigation extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
